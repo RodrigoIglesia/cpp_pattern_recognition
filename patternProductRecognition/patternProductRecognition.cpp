058fcbf566e2d5459dfc26567b6f00cacf9847a6
RodrigoIglesia <r.iglesia96@gmail.com>
2023-01-15 18:12:10 +0100

    Agregar archivos de proyecto.

---
+++ patternProductRecognition/patternProductRecognition.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp> // imshow
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp> // feature detector 
+#include <opencv2/opencv.hpp> // Homografía y transformacion perspectiva
+
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+    String folderpath = "C://INCARLOPSA//PROYECTOS//1_VISION_CERRADORAS//imgs//*.bmp";
+    vector<String> filenames;
+
+    glob(folderpath, filenames);
+    for (size_t i = 0; i < filenames.size(); i++)
+    {
+        // Imagen donde se detectaran los patrones
+        Mat img = imread(filenames[i], IMREAD_COLOR);
+        imshow("Imagen", img);
+        waitKey();
+    }
+
+    return 0;
+}
